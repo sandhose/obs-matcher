4b4513c2e417e4440aa1075867155491102e02a0
Quentin Gliech <quentingliech@gmail.com>
2018-06-08 10:09:06 +0200

    Get some config options from environment

--- matcher/config.py
+++ matcher/config.py
@@ -1,16 +1,44 @@
 import os
 
-# FIXME: instance config file
+from sqlalchemy.engine.url import URL
+
+
+def env_var(key, default=None):
+    if key + '_FILE' in os.environ:
+        with open(os.environ[key + '_FILE'], 'r') as file:
+            value = file.read().strip()
+    elif key in os.environ:
+        value = os.environ[key]
+    else:
+        value = default
+
+    if str(value).upper() == 'TRUE':
+        value = True
+    elif str(value).upper() == 'FALSE':
+        value = False
+
+    return value
+
+
+postgres_url = str(URL(
+    drivername='postgres+psycopg2',
+    host=env_var('POSTGRES_HOST', 'localhost'),
+    port=env_var('POSTGRES_PORT', '5432'),
+    username=env_var('POSTGRES_USERNAME'),
+    password=env_var('POSTGRES_PASSWORD'),
+    database=env_var('POSTGRES_DATABASE', 'matcher'),
+))
 
 
 class Config(object):
-    DEBUG = True
+    DEBUG = env_var('DEBUG', True)
     TESTING = False
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://localhost/matcher'
+    SQLALCHEMY_DATABASE_URI = env_var('SQLALCHEMY_DATABASE_URI', postgres_url)
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     DATABASE_CONNECT_OPTIONS = {}
 
     THREADS_PER_PAGE = 2
-    SECRET_KEY = 'Something reeaaally secret (wow, spooky.)'
+    SECRET_KEY = env_var('SECRET_KEY',
+                         'Something reeaaally secret (wow, spooky.)')
