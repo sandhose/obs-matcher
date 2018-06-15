9c3060a2174ae57889cddf605b26fe79c3f35bca
Quentin Gliech <quentingliech@gmail.com>
2018-06-15 13:55:20 +0200

    api: Move out PlatformGroups in own namepsace

--- matcher/api/__init__.py
+++ matcher/api/__init__.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
+
 from flask_restplus import Api
 
-from . import platforms
+from .namespaces import register_all
 
 blueprint = Blueprint('api_v2', __name__)
 api = Api(blueprint, version='1.0', title='Matcher API')
 
-api.add_namespace(platforms.api)
+register_all(api)
