9237c79d49478eb4ac8270de8cbb5679e4360d8f
Quentin Gliech <quentingliech@gmail.com>
2018-06-15 11:29:28 +0200

    api: Add group resource

--- matcher/api/pagination.py
+++ matcher/api/pagination.py
@@ -1,6 +1,7 @@
 import math
 from functools import wraps
-from flask_restplus import reqparse, fields, Model
+
+from flask_restplus import Model, fields, reqparse
 
 arguments = reqparse.RequestParser()
 arguments.add_argument('page', type=int, required=False, default=1)
@@ -13,20 +14,9 @@ model = Model('Pagination', {
     'pages': fields.Integer(description='Total number of pages of results'),
     'per_page': fields.Integer(description='Number of items per page of results'),
     'total': fields.Integer(description='Total number of results'),
+    'has_next': fields.Boolean(description='Does it have a next page?'),
+    'has_prev': fields.Boolean(description='Does it have a previous page?'),
 })
-
-
-def wrap_query(query):
-    args = arguments.parse_args()
-    start = (args['page'] - 1) * args['per_page']
-    end = start + args['per_page']
-    total = query.count()
-    return query[start:end], {
-        'page': args['page'],
-        'per_page': args['per_page'],
-        'total': total,
-        'pages': int(math.ceil(float(total) / args['per_page']))
-    }
 
 
 def wrap(api, _model):
@@ -41,10 +31,9 @@ def wrap(api, _model):
         @api.expect(arguments)
         @wraps(func)
         def f(*args, **kwargs):
-            query = func(*args, **kwargs)
-            query, pagination = wrap_query(query)
+            pagination = func(*args, **kwargs).paginate()
             return {
-                'items': query,
+                'items': pagination.items,
                 'pagination': pagination,
             }
         return f
