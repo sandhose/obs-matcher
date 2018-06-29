6e0719246d096db7a55b249d9f1f942e1150b956
Quentin Gliech <quentingliech@gmail.com>
2018-06-29 15:49:39 +0200

    Move dashboard templates inside matcher/dashboard/

--- matcher/dashboard/__init__.py
+++ matcher/dashboard/__init__.py
@@ -2,7 +2,8 @@ from flask import Blueprint
 
 from . import views
 
-blueprint = Blueprint('dashboard', __name__)
+blueprint = Blueprint('dashboard', __name__,
+                      template_folder='templates')
 
 blueprint.add_url_rule('/', view_func=views.home.HomeView.as_view('home'))
 blueprint.add_url_rule('/scraps', view_func=views.scraps.ScrapListView.as_view('scrap_list'))
