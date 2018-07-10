e2905e40fa752a71713d6dc57f5778bffbdfb814
Quentin Gliech <quentingliech@gmail.com>
2018-07-10 14:03:41 +0200

    Ensure that the tablefunc extension is created

--- matcher/scheme/__init__.py
+++ matcher/scheme/__init__.py
@@ -1,6 +1,8 @@
 from sqlalchemy import MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
+from .utils import ensure_extension
+
 convention = {
     "ix": 'ix_%(column_0_label)s',
     "uq": "uq_%(table_name)s_%(column_0_name)s",
@@ -12,3 +14,5 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+ensure_extension('tablefunc', metadata)
