8956509570bf1baa566e3a72a02e835a748a3a17
Alexandre Franke <alexandre.franke@coe.int>
2019-03-14 09:09:47 +0100

    Update compatibility information

--- setup.py
+++ setup.py
@@ -43,8 +43,8 @@ setup(
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     packages=find_packages(),
