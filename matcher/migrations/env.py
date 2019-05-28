ffa830a6263f3bfddc21fe7a10a20ac6146a9256
Alexandre Franke <alexandre.franke@coe.int>
2019-05-28 10:44:08 +0200

    Fix issue with % char in secrets

--- matcher/migrations/env.py
+++ matcher/migrations/env.py
@@ -20,7 +20,8 @@ fileConfig(config.config_file_name)
 logger = logging.getLogger("alembic.env")
 
 config.set_main_option(
-    "sqlalchemy.url", current_app.config.get("SQLALCHEMY_DATABASE_URI")
+    "sqlalchemy.url",
+    current_app.config.get("SQLALCHEMY_DATABASE_URI").replace("%", "%%"),
 )
 target_metadata = Base.metadata
 
