d3f07c92675dfe0309e301452ff2f229fa299b28
Alexandre Franke <alexandre.franke@coe.int>
2019-06-27 17:37:32 +0200

    style: blackened and retabed

--- scripts/import_replay.py
+++ scripts/import_replay.py
@@ -1,14 +1,15 @@
 from pathlib import Path
 import json, io, shutil
 from matcher.scheme.import_ import ImportFile
-from matcher.scheme.platform import Session
+from matcher.scheme.platform import Platform, Session
+from matcher.scheme.provider import Provider
 from matcher.scheme.enums import *
 from matcher.app import db
 from matcher import app
 from matcher.tasks.import_ import process_file
 
-with io.open('/tmp/files.json', 'r', encoding='utf-8') as f:
-	files = json.load(f)
+with io.open("/tmp/files.json", "r", encoding="utf-8") as f:
+    files = json.load(f)
 
 #  {
 #    "id": 130,
@@ -39,34 +40,44 @@ with io.open('/tmp/files.json', 'r', encoding='utf-8') as f:
 app.app_context().push()
 
 
-
 for f in files:
-	p = Path('/var/cache/scraping/production/imports') / (str(f['id']) + '.csv')	
-	if not p.exists():
-		print("skipping", f)
-		continue
+    p = Path("/var/cache/scraping/production/imports") / (str(f["id"]) + ".csv")
+    if not p.exists():
+        print("skipping", f)
+        continue
 
-	new_file = ImportFile(
-		filename=f['filename'],
-		fields=f['field'],
-		imported_external_object_type=ExternalObjectType.from_name(f["imported_external_object_type"]),
-		platform_id=f["platform_id"],
-		status=ImportFileStatus.UPLOADED,
-		provider_id=f["provider_id"]
-	)
+    provider_id = f["provider_id"]
+    platform_id = f["platform_id"]
+    try:
+        provider = db.session.query(Provider).filter(Provider.id == provider_id).one()
+        platform = db.session.query(Platform).filter(Platform.id == platform_id).one()
+    except:
+        print(f"Provider or platform not found, skipping {f}.")
+        continue
 
-	for s in f["sessions"]:
-		ses = db.session.query(Session).get(s)
-		if ses is None:
-			print("Could not find session", s, f)
-			continue
-		new_file.sessions.append(ses)
+    new_file = ImportFile(
+        filename=f["filename"],
+        fields=f["field"],
+        imported_external_object_type=ExternalObjectType.from_name(
+            f["imported_external_object_type"]
+        ),
+        platform_id=f["platform_id"],
+        status=ImportFileStatus.UPLOADED,
+        provider_id=f["provider_id"],
+    )
 
-	db.session.add(new_file)
-	db.session.commit()
+    for s in f["sessions"]:
+        ses = db.session.query(Session).get(s)
+        if ses is None:
+            print("Could not find session", s, f)
+            continue
+        new_file.sessions.append(ses)
 
-	shutil.copyfile(p, new_file.path)
+    db.session.add(new_file)
+    db.session.commit()
 
-	t = process_file.apply_async((new_file.id, ))
+    shutil.copyfile(p, new_file.path)
 
-	print(new_file.id, t.task_id)
+    t = process_file.apply_async((new_file.id,))
+
+    print(new_file.id, t.task_id)
