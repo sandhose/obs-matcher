dbbd11b26f06552bfe7cfe101eadd5cef4e38eb1
Quentin Gliech <quentingliech@gmail.com>
2020-01-24 17:02:37 +0100

    fix isort

--- matcher/__init__.py
+++ matcher/__init__.py
@@ -1,21 +1,15 @@
-from gevent.monkey import patch_all
+from gevent.monkey import patch_all; patch_all()  # noqa: E702
+from psycogreen.gevent import patch_psycopg; patch_psycopg()  # noqa: E702
 
-patch_all()
-from psycogreen.gevent import patch_psycopg  # noqa: E402
+from celery import Celery
+from werkzeug.middleware.proxy_fix import ProxyFix
 
-patch_psycopg()
-
-from werkzeug.middleware.proxy_fix import ProxyFix  # noqa: E402
-from celery import Celery  # noqa: E402
-
-from .app import create_app, injector  # noqa: E402
-from .countries import load_data  # noqa: E402
-
+from .app import create_app, injector
+from .countries import load_data
 
 app = create_app()
 celery = injector.get(Celery)
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
-
 
 load_data(app)
