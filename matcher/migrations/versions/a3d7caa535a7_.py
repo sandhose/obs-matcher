ab18f3057f2dedc960fbcf8025fa8c71afb9bdde
Alexandre Franke <alexandre.franke@coe.int>
2020-04-15 16:26:07 +0200

    style: black and isort

--- matcher/migrations/versions/a3d7caa535a7_.py
+++ matcher/migrations/versions/a3d7caa535a7_.py
@@ -6,19 +6,27 @@ Create Date: 2020-03-30 14:59:39.130111
 
 """
 from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
-revision = 'a3d7caa535a7'
-down_revision = '01cc4353c886'
+revision = "a3d7caa535a7"
+down_revision = "01cc4353c886"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.sync_enum_values('public', 'platformtype', ['GLOBAL', 'INFO', 'SVOD', 'TVOD'], ['AVOD', 'GLOBAL', 'INFO', 'SVOD', 'TVOD'])
+    op.sync_enum_values(
+        "public",
+        "platformtype",
+        ["GLOBAL", "INFO", "SVOD", "TVOD"],
+        ["AVOD", "GLOBAL", "INFO", "SVOD", "TVOD"],
+    )
 
 
 def downgrade():
-    op.sync_enum_values('public', 'platformtype', ['AVOD', 'GLOBAL', 'INFO', 'SVOD', 'TVOD'], ['GLOBAL', 'INFO', 'SVOD', 'TVOD'])
+    op.sync_enum_values(
+        "public",
+        "platformtype",
+        ["AVOD", "GLOBAL", "INFO", "SVOD", "TVOD"],
+        ["GLOBAL", "INFO", "SVOD", "TVOD"],
+    )
